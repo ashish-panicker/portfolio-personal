2d7382c91ac83f8c65b35ea45d64525b8b96c92f
ashish-panicker <ashish.s.panicker@proton.me>
2024-07-27 17:03:31 +0530

    More fixes to the contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -72,12 +72,13 @@ export default function Services() {
         opacity: 1,
         transition: { delay: 2, duration: 0.4, ease: 'easeIn' },
       }}
+      exit={{ opacity: 0 }}
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-8">
-          <div className="xl:h-[54%] order-2 xl:order-1 w-[640px]">
+          <div className="xl:h-[54%] xl:w-[640px] order-2">
             <div className="flex flex-col gap-6 md:p-6 py-2 px-2 bg-card/20 rounded-2xl">
-              <h3 className="text-3xl text-primary/90">
+              <h3 className="md:text-3xl text-primary/90">
                 Let&apos;s work together
               </h3>
               <p className="hidden md:block">
@@ -86,7 +87,7 @@ export default function Services() {
               <div>
                 <Form {...form}>
                   <form onSubmit={form.handleSubmit(onSubmit)}>
-                    <div className="grid grid-cols-1 gap-4 mb-8">
+                    <div className="grid grid-cols-1 gap-4 md:mb-8">
                       <FormField
                         control={form.control}
                         name="fullname"
@@ -238,7 +239,10 @@ export default function Services() {
                         )}
                       />
                     </div>
-                    <Button type="submit" className="font-semibold">
+                    <Button
+                      type="submit"
+                      className="font-semibold mt-3 md:mt-0"
+                    >
                       Let us connect
                     </Button>
                   </form>
@@ -246,17 +250,17 @@ export default function Services() {
               </div>
             </div>
           </div>
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            <ul className="flex flex-col gap-10">
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none md:mb-8 xl:mb-0">
+            <ul className="flex flex-col md:gap-10 gap-1">
               {info.map((i, index) => {
                 return (
-                  <li key={index} className="flex items-center gap-6">
-                    <div className="w-[54px] xl:w-[72px] h-[54px] xl:h-[72px] flex items-center justify-center text-2xl rounded-lg bg-primary/30">
-                      {i.icon}
+                  <li key={index} className="flex items-center gap-3">
+                    <div className="p-2 w-[36px] xl:w-[72px] h-[36px] xl:h-[72px] flex items-center justify-center md:text-2xl rounded-lg bg-primary/30">
+                      {i.icon} <span></span>
                     </div>
                     <div>
                       {/* <p>{i.title}</p> */}
-                      <h3 className="text-xl">{i.text}</h3>
+                      <h3 className="md:text-xl">{i.text}</h3>
                     </div>
                   </li>
                 )
--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
-import { Inter } from 'next/font/google'
+import { Roboto_Mono } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/shared/Header'
 import PageTransition from '@/components/shared/PageTransition'
 import StairTransition from '@/components/shared/StairTransition'
 
-const font = Inter({
+const font = Roboto_Mono({
   subsets: ['latin'],
 })
 
@@ -20,6 +20,9 @@ export default function RootLayout({ children }) {
         <Header />
         <StairTransition />
         <PageTransition>{children}</PageTransition>
+        <footer className="p-2 text-center bg-primary/10 fixed right-0 bottom-0">
+          <p className="text-sm">Ashish S</p>
+        </footer>
       </body>
     </html>
   )
